0a0be7d318c825b405c40fb107ea061d719d6120
Ryan Kadri <ryankadri.dev@gmail.com>
2023-03-08 10:35:36 +0100

    :lipstick: add angular font and films exemple

--- src/app/film.ts
+++ src/app/film.ts
@@ -4,3 +4,37 @@ interface Film {
     director: string;
     actors: string[];
 }
+
+const FILMS: Film[] = [
+    {
+        "title": "Inception",
+        "year": 2010,
+        "director": "Christopher Nolan",
+        "actors": [
+            "Joseph Gordon-Levitt",
+            "Leonardo DiCaprio",
+            "Ellen Page"
+        ]
+    }, {
+        "title": "The dark Knight rises",
+        "year": 2012,
+        "director": "Christopher Nolan",
+        "actors": [
+            "Christian Bale",
+            "Michael Caine",
+            "Gary Oldman",
+            "Marion Cotillard",
+            "Joseph Gordon-Levitt"
+        ]
+    }, {
+        "title": "Child 44",
+        "year": 2015,
+        "director": "Daniel Espinosa",
+        "actors": [
+            "Tom Hardy",
+            "Noomi Rapace",
+            "Gary Oldman",
+            "Vincent Cassel"
+        ]
+    }
+];
